d91ada44bdd8ee4055c9cf72398907f121257067
liguiyuan <820001401@qq.com>
2020-04-02 18:44:38 +0800

    add fasterRCNN

--- net/detector.py
+++ net/detector.py
@@ -13,6 +13,10 @@ from bbox_tools import generate_anchors
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision.models.detection.rpn import RegionProposalNetwork
 from torchvision.models.detection.rpn import RPNHead
+from torchvision.ops import MultiScaleRoIAlign
+from torchvision.models.detection.roi_heads import RoIHeads
+from torchvision.models.detection.transform import GeneralizedRCNNTransform
+from torchvision.models.detection.generalized_rcnn import GeneralizedRCNN
 
 class CEM(nn.Module):
     def __init__(self):
@@ -140,8 +144,11 @@ def detecter():
     rcnn_output = rcnn(feature_roi)
     
 
-class ThunderNet(nn.Module):
-    def __init__(self, backbone,
+class ThunderNet(GeneralizedRCNN):
+    def __init__(self, backbone, num_classes=None,
+        # transform parameters
+        min_size=800, max_size=1333,
+        image_mean=None, image_std=None,
         # RPN parameters
         rpn_anchor_generator=None, rpn_head=None,
         rpn_pre_nms_top_n_train=2000, rpn_pre_nms_top_n_test=100,
@@ -151,8 +158,12 @@ class ThunderNet(nn.Module):
         rpn_fg_iou_thresh=0.7, rpn_bg_iou_thresh=0.3,
         rpn_batch_size_per_image=256, rpn_positive_fraction=0.5,
 
-        ):
-        super(ThunderNet, self).__init__()
+        # Box parameters
+        box_roi_pool=None, box_head=None, box_predictor=None,
+        box_score_thresh=0.05, box_nms_thresh=0.5, box_detections_per_img=100,
+        box_fg_iou_thresh=0.5,box_bg_iou_thresh=0.5,
+        box_batch_size_per_image=512, box_positive_fraction=0.25,
+        bbox_reg_weights=None):
 
         if not hasattr(backbone, "out_channels"):
             raise ValueError(
@@ -161,7 +172,16 @@ class ThunderNet(nn.Module):
                 "same for all the levels)")
 
         assert isinstance(rpn_anchor_generator, (AnchorGenerator, type(None)))
-        
+        assert isinstance(box_roi_pool, (MultiScaleRoIAlign, type(None)))
+
+        if num_classes is not None:
+            if box_predictor is not None:
+                raise ValueError("num_classes should be None when box_predictor is specified")
+        else:
+            if box_predictor is None:
+                raise ValueError("num_classes should not be None when box_predictor "
+                                 "is not specified")
+
         out_channels = backbone.out_channels
 
         if rpn_anchor_generator is None:
@@ -183,6 +203,89 @@ class ThunderNet(nn.Module):
             rpn_batch_size_per_image, rpn_positive_fraction,
             rpn_pre_nms_top_n, rpn_post_nms_top_n, rpn_mns_thresh)
 
+
+        # box
+        if box_roi_pool is None:
+            box_roi_pool = MultiScaleRoIAlign(
+                featmap_names=['0', '1', '2', '3'],
+                output_size=7,
+                sampling_ratio=2)
+
+        if box_head is None:
+            resolution = box_roi_pool.output_size[0]
+            representation_size = 1024
+            box_head = TwoMLPHead(
+                out_channels * resolution ** 2,
+                representation_size)
+
+        if box_predictor is None:
+            representation_size = 1024
+            box_predictor = FastRCNNPredictor(
+                representation_size,
+                num_classes)
+
+        roi_heads = RoIHeads(
+            # Box
+            box_roi_pool, box_head, box_predictor,
+            box_fg_iou_thresh, box_bg_iou_thresh,
+            box_batch_size_per_image, box_positive_fraction,
+            bbox_reg_weights,
+            box_score_thresh, box_nms_thresh, box_detections_per_img)
+
+
+        if image_mean is None:
+            image_mean = [0.485, 0.456, 0.406]
+        if image_std is None:
+            image_std = [0.229, 0.224, 0.225]
+        transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std)
+
+        super(ThunderNet, self).__init__(backbone, rpn, roi_heads, transform)
+
+class TwoMLPHead(nn.Module):
+    """
+    Standard heads for FPN-based models
+    Arguments:
+        in_channels (int): number of input channels
+        representation_size (int): size of the intermediate representation
+    """
+
+    def __init__(self, in_channels, representation_size):
+        super(TwoMLPHead, self).__init__()
+        self.fc6 = nn.Linear(in_channels, representation_size)
+        self.fc7 = nn.Linear(representation_size, representation_size)
+
+    def forward(self, x):
+        x = x.flatten(start_dim=1)
+
+        x = F.relu(self.fc6(x))
+        x = F.relu(self.fc7(x))
+
+        return x
+  
+class FastRCNNPredictor(nn.Module):
+    """
+    Standard classification + bounding box regression layers
+    for Fast R-CNN.
+    Arguments:
+        in_channels (int): number of input channels
+        num_classes (int): number of output classes (including background)
+    """
+    def __init__(self, in_channels, num_classes):
+        super(FastRCNNPredictor, self).__init__()
+        self.cls_score = nn.Linear(in_channels, num_classes)
+        self.bbox_pred = nn.Linear(in_channels, num_classes * 4)
+
+    def forward(self, x):
+        if x.dim() == 4:
+            assert list(x.shape[2:]) == [1, 1]
+        x = x.flatten(start_dim=1)
+        scores = self.cls_score(x)
+        bbox_deltas = self.bbox_pred(x)
+
+        return scores, bbox_deltas
+        
+
+
 if __name__ == '__main__':
     #detecter()
 
@@ -190,5 +293,6 @@ if __name__ == '__main__':
     snet.out_channels = 1024
 
 
-    thundernet = ThunderNet(snet)
+    thundernet = ThunderNet(snet, num_classes=2)
+    print('thundernet: ', thundernet)
     
