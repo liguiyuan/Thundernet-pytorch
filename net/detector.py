d05003ee3adf83da7d7de6e3de91bf34c060fa55
liguiyuan <820001401@qq.com>
2020-04-02 17:29:14 +0800

    create thundernet

--- net/detector.py
+++ net/detector.py
@@ -8,137 +8,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from Snet49 import ShuffleNetV2
 from roi_layers.ps_roi_align import PSRoIAlign
+from bbox_tools import generate_anchors
 
-"""
-class PSRoiAlignPooling(nn.Module):
-    def __init__(self, pool_size, num_rois, alpha, **kwargs):
-        super(PSRoiAlignPooling, self).__init__()
-        self.pool_size = pool_size
-        self.num_rois = num_rois    # number of rois
-        self.alpha_channels = alpha
-
-    def build(self, input_shape):
-        self.nb_channels = input_shape[0][3]    # ?
-
-    def compute_output_shape(self, input_shape):
-        return None, self.num_rois, self.pool_size, self.pool_size, self.alpha_channels
-
-    def forward(self, x, mask=None):
-        assert(len(x) == 2)
-        total_bins = 1
-        img = x[0]      # x[0] is image shape(rows, cols, channels)
-
-        rois = x[1]     # x[1] is roi shape(num_rois, 4) with ordering (x, y, w, h)
- 
-        bin_crop_size = []
-        for num_bins, crop_dim in zip((7, 7), (14, 14)):
-            assert num_bins >= 1
-            assert crop_dim % num_bins == 0
-            total_bins *= num_bins
-            bin_crop_size.append(crop_dim // num_bins)
-
-         xmin, ymin, xmax, ymax = torch.unbind(rois[0], dim=1)
-         spatial_bins_y =  spatial_bins_x = 7
-         step_y = (ymax - ymin) / spatial_bins_y
-         step_x = (xmax - xmin) / spatial_bins_x
-
-         # gen bins
-         position_sensitive_boxes = []
-         for bin_x in range(self.pool_size):
-            for bin_y in range(self.pool_size):
-                box_coordinates = [
-                    ymin + bin_y * step_y,
-                    xmin + bin_x * step_x,
-                    ymin + (bin_y + 1) * step_y,
-                    xmin + (bin_x + 1) * step_x 
-                ]
-                position_sensitive_boxes.append(torch.stack(box_coordinates, dim=1))
-
-        img_splits = torch.split(img, total_bins, dim=3)
-        box_image_indices = np.zeros(self.num_rois)
-
-        feature_crops = []
-        for split, box in zip(img_splits, position_sensitive_boxes):
-            crop = CropAndResizeFunction.apply(split, box, box_image_indices, bin_crop_size[0], bin_crop_size[1], 0)
-
-            crop_1 = torch.max(crop, dim=1, keepdim=False, out=None)
-            crop_2 = torch.max(crop, dim=2, keepdim=False, out=None)
-            crop = torch.stack(crop_1, crop_2)
-            crop = crop.unsqueeze(1)
-
-            feature_crops.append(crop)
-
-        final_output = torch.cat(feature_crops, dim=1)
-
-        final_output = final_output.reshape(1, self.num_rois, self.pool_size, self.pool_size, self.alpha_channels)
-
-        final_output = final_output.permute(0, 1, 2, 3, 4)
-
-        return final_output
-
-
-
-class RPN(nn.Module):
-    def __init__(self, in_channels, num_anchors, nb_classes, in_channels2):
-        super(RPN, self).__init__()
-        #rpn part
-
-        # SAM module
-        #self.conv1 = Conv_1x1(in_channels=in_channels, out_channels=245, strides=1, groups=1)
-        self.conv1 = nn.Conv2d(245, 245, 1, 1, 0, bias=False) # input channel = 245 ?
-        self.bn1 = nn.BatchNorm2d(245)
-
-        # RPN
-        self.dw5x5 = nn.Conv2d(245, 245, kernel_size=5, stride=1, padding=1, groups=245)
-        self.conv1_1 = nn.Conv2d(245, 256, kernel_size=1, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(num_anchors, (1, 1))         # class
-        self.conv3 = nn.Conv2d(num_anchors * 4, (1, 1))     # region
-
-        # classifier part
-        self.dropout = nn.Dropout(0.5)
-        self.batchnorm = nn.BatchNorm2d()
-        self.linear = nn.Linear(1024)
-        self.linear_cls = nn.Linear(nb_classes)
-        self.softmax = nn.Softmax()
-        self.linear_reg = nn.Linear(4 * (nb_classes - 1))
-
-    def forward(self, x):   # x: CEM output feature (20x20x245)
-        # RPN
-        x = self.dw5x5(x)
-        x = self.conv1_1(x)
-        x_class = F.sigmoid(self.conv2(x))
-        x_regr = self.conv3(x)
-
-        return [x_class, x_regr]
-
-    def classifier(self, input, input_rois, num_rois, nb_classes=3):
-        # SAM module
-        feature_cem = input[0]      # feature map of CEM: 20x20x5
-        feature_rpn = input[1]      # feature map of RPN
-
-        rpn = self.conv1(rpn)
-        rpn = self.bn1(rpn)
-        rpn = F.sigmoid(rpn)
-        sam_output = feature_cem * rpn
-        
-        # PSRoI align
-        pooling_regions = 7
-        alpha = 5
-        out_roi_pool = PSRoiAlignPooling(pooling_regions, num_rois, alpha)([sam_output, input_rois])
-
-        # fc
-        out = torch.flatten(out_roi_pool)   
-        out = self.linear(out)                  # output 1-dim: 1024
-        #out = self.dropout(out)                # dropout
-
-        out_score = self.linear_cls(out)        # 
-        out_class = self.softmax(out_score)     # classification
-
-        out_regr = self.linear_reg(out)         # localization
-
-        return [out_class, out_regr]
-"""
-#nn.Conv2d(oup_inc, oup_inc, 1, 1, 0, bias=False)
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.models.detection.rpn import RegionProposalNetwork
+from torchvision.models.detection.rpn import RPNHead
 
 class CEM(nn.Module):
     def __init__(self):
@@ -196,6 +70,8 @@ class RPN(nn.Module):
         self.bn1 = nn.BatchNorm2d(256)
         #self.conv2 = nn.Conv2d(num_anchors, (1, 1))         # class
         #self.conv3 = nn.Conv2d(num_anchors * 4, (1, 1))     # region
+
+        anchor_generator = generate_anchors()
 
     def forward(self, x):   # x: CEM output feature (20x20x245)
         # RPN
@@ -264,7 +140,55 @@ def detecter():
     rcnn_output = rcnn(feature_roi)
     
 
+class ThunderNet(nn.Module):
+    def __init__(self, backbone,
+        # RPN parameters
+        rpn_anchor_generator=None, rpn_head=None,
+        rpn_pre_nms_top_n_train=2000, rpn_pre_nms_top_n_test=100,
+        rpn_post_nms_top_n_train=2000, rpn_post_nms_top_n_test=1000,
+
+        rpn_mns_thresh=0.7,
+        rpn_fg_iou_thresh=0.7, rpn_bg_iou_thresh=0.3,
+        rpn_batch_size_per_image=256, rpn_positive_fraction=0.5,
+
+        ):
+        super(ThunderNet, self).__init__()
+
+        if not hasattr(backbone, "out_channels"):
+            raise ValueError(
+                "backbone should contain an attribute out_channels "
+                "specifying the number of output channels (assumed to be the "
+                "same for all the levels)")
+
+        assert isinstance(rpn_anchor_generator, (AnchorGenerator, type(None)))
+        
+        out_channels = backbone.out_channels
+
+        if rpn_anchor_generator is None:
+            anchor_sizes = ((32,), (64,), (128,), (256,), (512,))
+            aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+            rpn_anchor_generator = AnchorGenerator(sizes=anchor_sizes, aspect_ratios=aspect_ratios)
+        
+        if rpn_head is None:
+            rpn_head = RPNHead(
+                out_channels, rpn_anchor_generator.num_anchors_per_location()[0]
+            )
+
+        rpn_pre_nms_top_n = dict(training=rpn_pre_nms_top_n_train, testing=rpn_pre_nms_top_n_test)
+        rpn_post_nms_top_n = dict(training=rpn_post_nms_top_n_train, testing=rpn_post_nms_top_n_test)
+
+        rpn = RegionProposalNetwork(
+            rpn_anchor_generator, rpn_head,
+            rpn_fg_iou_thresh, rpn_bg_iou_thresh,
+            rpn_batch_size_per_image, rpn_positive_fraction,
+            rpn_pre_nms_top_n, rpn_post_nms_top_n, rpn_mns_thresh)
 
 if __name__ == '__main__':
-    detecter()
+    #detecter()
+
+    snet = ShuffleNetV2()
+    snet.out_channels = 1024
+
+
+    thundernet = ThunderNet(snet)
     
--- net/rpn.py
+++ net/rpn.py
@@ -1,84 +1,200 @@
 import numpy as np
-import torch
-from torch import nn
 from torch.nn import functional as F
+import torch as t
+from torch import nn
 
-from bbox_tools import generate_anchors
-import _utils as det_utils
+from utils.bbox_tools import generate_anchor_base
+from utils.creator_tool import ProposalCreator
 
-class RPN(nn.Module):
-    def __init__(self, anchor_scales=[8, 16, 32], ratios=[0.5, 1, 2], feat_stride=16,
-        proposal_creator_params=dict()):
-        super(RPN, self).__init__()
-
-        self.anchor_base = generate_anchors(ratios=ratios)
-        self.feat_stride = feat_stride
-        self.proposal_layer = ProposalCreator(self, **proposal_creator_params)
-
-    def forward(self, x):   # x: CEM output feature (20x20x245)
-        pass
-        #n, _, hh, ww = x.shape
-        #anchor = _enumerate_shifted_anchor(
-        #    np.array(self.anchor_base), self.feat_stride, hh, ww)
-      
 
 class RegionProposalNetwork(nn.Module):
-    __annotations__ = {
-        'box_coder': det_utils.BoxCoder,
-        'proposal_matcher': det_utils.Matcher,
-        'fg_bg_sampler': det_utils.BalancedPositiveNegativeSampler,
-        'pre_nms_top_n': Dict[str, int],
-        'post_nms_top_n': Dict[str, int],
-    }
+    """Region Proposal Network introduced in Faster R-CNN.
+    This is Region Proposal Network introduced in Faster R-CNN [#]_.
+    This takes features extracted from images and propose
+    class agnostic bounding boxes around "objects".
+    .. [#] Shaoqing Ren, Kaiming He, Ross Girshick, Jian Sun. \
+    Faster R-CNN: Towards Real-Time Object Detection with \
+    Region Proposal Networks. NIPS 2015.
+    Args:
+        in_channels (int): The channel size of input.
+        mid_channels (int): The channel size of the intermediate tensor.
+        ratios (list of floats): This is ratios of width to height of
+            the anchors.
+        anchor_scales (list of numbers): This is areas of anchors.
+            Those areas will be the product of the square of an element in
+            :obj:`anchor_scales` and the original area of the reference
+            window.
+        feat_stride (int): Stride size after extracting features from an
+            image.
+        initialW (callable): Initial weight value. If :obj:`None` then this
+            function uses Gaussian distribution scaled by 0.1 to
+            initialize weight.
+            May also be a callable that takes an array and edits its values.
+        proposal_creator_params (dict): Key valued paramters for
+            :class:`model.utils.creator_tools.ProposalCreator`.
+    .. seealso::
+        :class:`~model.utils.creator_tools.ProposalCreator`
+    """
 
-    def __init__(self, 
-                 anchor_generator, 
-                 head, 
-                 fg_iou_thresh, bg_iou_thresh,
-                 batch_size_per_image, positive_fraction,
-                 pre_nms_top_n, pos_nms_top_n, nms_thresh):
+    def __init__(
+            self, in_channels=512, mid_channels=512, ratios=[0.5, 1, 2],
+            anchor_scales=[8, 16, 32], feat_stride=16,
+            proposal_creator_params=dict(),
+    ):
         super(RegionProposalNetwork, self).__init__()
-        self.anchor_generator = anchor_generator
-        self.head = head
-        self.box_coder = det_utils.BoxCoder(weights=(1.0, 1.0, 1.0, 1.0))
+        self.anchor_base = generate_anchor_base(
+            anchor_scales=anchor_scales, ratios=ratios)
+        self.feat_stride = feat_stride
+        self.proposal_layer = ProposalCreator(self, **proposal_creator_params)
+        n_anchor = self.anchor_base.shape[0]
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, 3, 1, 1)
+        self.score = nn.Conv2d(mid_channels, n_anchor * 2, 1, 1, 0)
+        self.loc = nn.Conv2d(mid_channels, n_anchor * 4, 1, 1, 0)
+        normal_init(self.conv1, 0, 0.01)
+        normal_init(self.score, 0, 0.01)
+        normal_init(self.loc, 0, 0.01)
 
-        # used during training
-        self.box_similarity = box_ops.box_iou
+    def forward(self, x, img_size, scale=1.):
+        """Forward Region Proposal Network.
+        Here are notations.
+        * :math:`N` is batch size.
+        * :math:`C` channel size of the input.
+        * :math:`H` and :math:`W` are height and witdh of the input feature.
+        * :math:`A` is number of anchors assigned to each pixel.
+        Args:
+            x (~torch.autograd.Variable): The Features extracted from images.
+                Its shape is :math:`(N, C, H, W)`.
+            img_size (tuple of ints): A tuple :obj:`height, width`,
+                which contains image size after scaling.
+            scale (float): The amount of scaling done to the input images after
+                reading them from files.
+        Returns:
+            (~torch.autograd.Variable, ~torch.autograd.Variable, array, array, array):
+            This is a tuple of five following values.
+            * **rpn_locs**: Predicted bounding box offsets and scales for \
+                anchors. Its shape is :math:`(N, H W A, 4)`.
+            * **rpn_scores**:  Predicted foreground scores for \
+                anchors. Its shape is :math:`(N, H W A, 2)`.
+            * **rois**: A bounding box array containing coordinates of \
+                proposal boxes.  This is a concatenation of bounding box \
+                arrays from multiple images in the batch. \
+                Its shape is :math:`(R', 4)`. Given :math:`R_i` predicted \
+                bounding boxes from the :math:`i` th image, \
+                :math:`R' = \\sum _{i=1} ^ N R_i`.
+            * **roi_indices**: An array containing indices of images to \
+                which RoIs correspond to. Its shape is :math:`(R',)`.
+            * **anchor**: Coordinates of enumerated shifted anchors. \
+                Its shape is :math:`(H W A, 4)`.
+        """
+        n, _, hh, ww = x.shape
+        anchor = _enumerate_shifted_anchor(
+            np.array(self.anchor_base),
+            self.feat_stride, hh, ww)
 
-        self.proposal_matcher = det_utils.Matcher(
-            fg_iou_thresh,
-            bg_iou_thresh,
-            allow_low_quality_matches=True,
-        )
+        n_anchor = anchor.shape[0] // (hh * ww)
+        h = F.relu(self.conv1(x))
 
-        self.fg_bg_sampler = det_utils.BalancedPositiveNegativeSampler(
-            batch_size_per_image, positive_fraction
-        )
-        # used during testing
-        self._pre_nms_top_n = pre_nms_top_n
-        self._post_nms_top_n = post_nms_top_n
-        self.nms_thresh = nms_thresh
-        self.min_size = 1e-3
+        rpn_locs = self.loc(h)
+        # UNNOTE: check whether need contiguous
+        # A: Yes
+        rpn_locs = rpn_locs.permute(0, 2, 3, 1).contiguous().view(n, -1, 4)
+        rpn_scores = self.score(h)
+        rpn_scores = rpn_scores.permute(0, 2, 3, 1).contiguous()
+        rpn_softmax_scores = F.softmax(rpn_scores.view(n, hh, ww, n_anchor, 2), dim=4)
+        rpn_fg_scores = rpn_softmax_scores[:, :, :, :, 1].contiguous()
+        rpn_fg_scores = rpn_fg_scores.view(n, -1)
+        rpn_scores = rpn_scores.view(n, -1, 2)
 
-    def pre_nms_top_n(self):
-        if self.training:
-            return self._pre_nms_top_n['training']
-        return self._pre_nms_top_n['testing']
+        rois = list()
+        roi_indices = list()
+        for i in range(n):
+            roi = self.proposal_layer(
+                rpn_locs[i].cpu().data.numpy(),
+                rpn_fg_scores[i].cpu().data.numpy(),
+                anchor, img_size,
+                scale=scale)
+            batch_index = i * np.ones((len(roi),), dtype=np.int32)
+            rois.append(roi)
+            roi_indices.append(batch_index)
 
-    def post_nms_top_n(self):
-        if self.training:
-            return self._post_nms_top_n['training']
-        return self._post_nms_top_n['testing']
+        rois = np.concatenate(rois, axis=0)
+        roi_indices = np.concatenate(roi_indices, axis=0)
+        return rpn_locs, rpn_scores, rois, roi_indices, anchor
 
 
-        
+def _enumerate_shifted_anchor(anchor_base, feat_stride, height, width):
+    # Enumerate all shifted anchors:
+    #
+    # add A anchors (1, A, 4) to
+    # cell K shifts (K, 1, 4) to get
+    # shift anchors (K, A, 4)
+    # reshape to (K*A, 4) shifted anchors
+    # return (K*A, 4)
+
+    # !TODO: add support for torch.CudaTensor
+    # xp = cuda.get_array_module(anchor_base)
+    # it seems that it can't be boosed using GPU
+    import numpy as xp
+    shift_y = xp.arange(0, height * feat_stride, feat_stride)
+    shift_x = xp.arange(0, width * feat_stride, feat_stride)
+    shift_x, shift_y = xp.meshgrid(shift_x, shift_y)
+    shift = xp.stack((shift_y.ravel(), shift_x.ravel(),
+                      shift_y.ravel(), shift_x.ravel()), axis=1)
+
+    A = anchor_base.shape[0]
+    K = shift.shape[0]
+    anchor = anchor_base.reshape((1, A, 4)) + \
+             shift.reshape((1, K, 4)).transpose((1, 0, 2))
+    anchor = anchor.reshape((K * A, 4)).astype(np.float32)
+    return anchor
+
+
+def _enumerate_shifted_anchor_torch(anchor_base, feat_stride, height, width):
+    # Enumerate all shifted anchors:
+    #
+    # add A anchors (1, A, 4) to
+    # cell K shifts (K, 1, 4) to get
+    # shift anchors (K, A, 4)
+    # reshape to (K*A, 4) shifted anchors
+    # return (K*A, 4)
+
+    # !TODO: add support for torch.CudaTensor
+    # xp = cuda.get_array_module(anchor_base)
+    import torch as t
+    shift_y = t.arange(0, height * feat_stride, feat_stride)
+    shift_x = t.arange(0, width * feat_stride, feat_stride)
+    shift_x, shift_y = xp.meshgrid(shift_x, shift_y)
+    shift = xp.stack((shift_y.ravel(), shift_x.ravel(),
+                      shift_y.ravel(), shift_x.ravel()), axis=1)
+
+    A = anchor_base.shape[0]
+    K = shift.shape[0]
+    anchor = anchor_base.reshape((1, A, 4)) + \
+             shift.reshape((1, K, 4)).transpose((1, 0, 2))
+    anchor = anchor.reshape((K * A, 4)).astype(np.float32)
+    return anchor
+
+
+def normal_init(m, mean, stddev, truncated=False):
+    """
+    weight initalizer: truncated normal and random normal.
+    """
+    # x is a parameter
+    if truncated:
+        m.weight.data.normal_().fmod_(2).mul_(stddev).add_(mean)  # not a perfect approximation
+    else:
+        m.weight.data.normal_(mean, stddev)
+        m.bias.data.zero_()
 
 
 if __name__ == '__main__':
-      rpn = RegionProposalNetwork()
+    ratios=[0.5, 1, 2]
+    anchor_scales=[8, 16, 32]
+    feat_stride = 16
 
-
-
-
-
-
+    rpn = RegionProposalNetwork(
+        512, 512,
+        ratios=ratios,
+        anchor_scales=anchor_scales,
+        feat_stride=feat_stride,
+    )
